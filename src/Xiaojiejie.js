64b0058fa62161c85b1508ea416f10840457c357
唐海岚 <gdx_lwx@qq.com>
2021-01-07 20:07:16 +0800

    提交test

--- src/Xiaojiejie.js
+++ src/Xiaojiejie.js
@@ -1,25 +1,27 @@
 import React,{ Component,Fragment } from "react";
-
+import './style.css'
 class Xiaojiejie extends Component{
     constructor(props){
         super(props)
         this.state={
             inputValue:'',
-            list:['基础按摩','精油推背']
+            list:['ServiceA','ServiceB']
         }
     }
     render(){
         return (
             // <div>
             <Fragment>
+                {/**注释  */}
                 <div>
-                    <input value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
+                    <label htmlFor="add">增加服务:</label>
+                    <input id="add" className="input" value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
                     <button onClick={this.addService.bind(this)}>增加服务</button>
                 </div>
                 <ul>
                    {
                        this.state.list.map((item,index)=>{
-                            return <li key={index+item} onClick={this.deleteItem.bind(this,index)}>{item}</li>
+                            return <li key={index+item} onClick={this.deleteItem.bind(this,index)} dangerouslySetInnerHTML={{__html:item}}></li>
                        })
                    }
                 </ul>
