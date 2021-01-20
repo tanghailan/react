e5c7ba0c6c9ce349ccd919bf5f373a9f55f1b37a
唐海岚 <gdx_lwx@qq.com>
2021-01-20 19:42:03 +0800

    提交

--- src/Xiaojiejie.js
+++ src/Xiaojiejie.js
@@ -1,55 +1,63 @@
-import React,{ Component,Fragment } from "react";
+import React, {Component, Fragment} from "react";
 import './style.css'
-class Xiaojiejie extends Component{
-    constructor(props){
+import XiaojiejieItem from "./XiaojiejieItem";
+
+class Xiaojiejie extends Component {
+    constructor(props) {
         super(props)
-        this.state={
-            inputValue:'',
-            list:['ServiceA','ServiceB']
+        this.state = {
+            inputValue: '',
+            list: ['基础按摩', '精油推背']
         }
     }
-    render(){
+
+    render() {
         return (
             // <div>
             <Fragment>
                 {/**注释  */}
                 <div>
                     <label htmlFor="add">增加服务:</label>
-                    <input id="add" className="input" value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
+                    <input id="add" className="input" value={this.state.inputValue}
+                           onChange={this.inputChange.bind(this)}/>
                     <button onClick={this.addService.bind(this)}>增加服务</button>
                 </div>
                 <ul>
-                   {
-                       this.state.list.map((item,index)=>{
-                            return <li key={index+item} onClick={this.deleteItem.bind(this,index)} dangerouslySetInnerHTML={{__html:item}}></li>
-                       })
-                   }
+                    {
+                        this.state.list.map((item, index) => {
+                            return
+                                {
+                                }
+                        })
+                    }
                 </ul>
             </Fragment>
             // </div>
 
         )
     }
-    inputChange(e){
+
+    inputChange(e) {
         this.setState({
-            inputValue:e.target.value
+            inputValue: e.target.value
         })
     }
+
     //增加列表
-    addService(){
+    addService() {
         this.setState({
-            list:[...this.state.list,this.state.inputValue],
-            inputValue:''
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ''
         })
     }
 
     //删除列表项
-    deleteItem(index){
+    deleteItem(index) {
         console.log(index)
         let list = this.state.list
-        list.splice(index,1);
+        list.splice(index, 1);
         this.setState({
-            list:list
+            list: list
         })
     }
 }
